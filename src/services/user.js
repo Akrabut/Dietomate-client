55c58d70b73185b04559f00e525bd8b27185a071
Or <orshadad@gmail.com>
2020-04-14 17:06:03 +0300

    start implementing login functionality

--- src/services/user.js
+++ src/services/user.js
@@ -3,9 +3,9 @@ import axios from 'axios'
 // TODO: TEMP WEBHOOK
 const ENDPOINT = 'https://webhook.site/1809822f-4c24-45c0-8639-274792004611'
 
-const sendDietForm = async (dietObject) => {
+const login = async (userObject) => {
   try {
-    const res = await axios.post(ENDPOINT, dietObject)
+    const res = await axios.post(ENDPOINT, userObject)
     return res.data
   } catch (err) {
     // TODO: SENSIBLE ERROR DISPLAY
@@ -14,5 +14,5 @@ const sendDietForm = async (dietObject) => {
 }
 
 export {
-  sendDietForm,
+  login,
 }
