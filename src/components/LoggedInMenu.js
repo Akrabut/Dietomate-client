8b6de65bd2d255d6bf36910ba5874a5fb961ef5f
Akraboot <orshadad@gmail.com>
2020-04-20 18:27:56 +0300

    add logout functionality

--- src/components/LoggedInMenu.js
+++ src/components/LoggedInMenu.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import { Menu } from 'semantic-ui-react'
+import { connect } from 'react-redux'
+import { logout } from '../actions/user'
 
-const LoggedInMenu = () => {
-  return null
+const LoggedInMenu = props => {
+  return (
+    <Menu.Menu
+      position='right'>
+      <Menu.Item
+        name='Log Out'
+        content='Log Out'
+        onClick={() => props.logout()}>
+      </Menu.Item>
+    </Menu.Menu>
+  )
 }
 
-export default LoggedInMenu
+const mapDispatchToProps = {
+  logout,
+}
+
+const connectedLoggedInMenu = connect(null, mapDispatchToProps)(LoggedInMenu)
+export default connectedLoggedInMenu
