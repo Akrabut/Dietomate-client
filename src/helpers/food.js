671cfeb892eede312fe4947e785b60cc257796da
Akraboot <orshadad@gmail.com>
2020-04-22 02:45:52 +0300

    define macro and micronurtient daily requirements

---
+++ src/helpers/food.js
@@ -0,0 +1,111 @@
+const apple = {
+  calories_per_100g: 52,
+  serving_size: 182,
+  macronutrients_per_100g: {
+    carbohydrate: 11.4,
+    protein: 0.3,
+    fat: 0.2,
+    fiber: 2.4
+  },
+  vitamin_per_100g: {
+    vitamin_a: {
+      amount: 54,
+      unit: 'IU'
+    },
+    vitamin_b1: {
+      amount: 0,
+      unit: 'mg'
+    },
+    vitamin_b2: {
+      amount: 0,
+      unit: 'mg'
+    },
+    vitamin_b3: {
+      amount: 0.1,
+      unit: 'mg'
+    },
+    vitamin_b5: {
+      amount: 0.1,
+      unit: 'mg'
+    },
+    vitamin_b6: {
+      amount: 0.0,
+      unit: 'mg'
+    },
+    vitamin_b7: {
+      amount: 0,
+      unit: 'mg'
+    },
+    vitamin_b9: {
+      amount: 3,
+      unit: 'mcg'
+    },
+    vitamin_b12: {
+      amount: 0,
+      unit: 'mcg'
+    },
+    vitamin_c: {
+      amount: 4.6,
+      unit: 'mg'
+    },
+    vitamin_d: {
+      amount: 0,
+      unit: 'IU'
+    },
+    vitamin_e: {
+      amount: 0.2,
+      unit: 'mg'
+    },
+    vitamin_k: {
+      amount: 2.2,
+      unit: 'mcg'
+    },
+  },
+  minerals_per_100g: {
+    calcium: {
+      amount: 6,
+      unit: 'mg'
+    },
+    iron: {
+      amount: 0.1,
+      unit: 'mg'
+    },
+    magnesium: {
+      amount: 5,
+      unit: 'mg'
+    },
+    phosphorus: {
+      amount: 11,
+      unit: 'mg'
+    },
+    potassium: {
+      amount: 107,
+      unit: 'mg'
+    },
+    sodium: {
+      amount: 1,
+      unit: 'mg'
+    },
+    zinc: {
+      amount: 0,
+      unit: 'mg'
+    },
+    copper: {
+      amount: 0,
+      unit: 'mg'
+    },
+    manganese: {
+      amount: 0,
+      unit: 'mg'
+    },
+    selenium: {
+      amount: 0,
+      unit: 'mcg'
+    },
+    fluoride: {
+      amount: 3.3,
+      unit: 'mcg'
+    }
+  },
+  category: 'fruit'
+}
