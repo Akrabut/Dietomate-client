1b4ab494966e536118d2d22aa4acd8b163ed85ae
Akraboot <orshadad@gmail.com>
2020-04-25 18:22:50 +0300

    user _id is sent from server on login and registeration and is saved in local storage

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -1,10 +1,12 @@
 const setUserInLocalStorage = userObject => {
+  localStorage.setItem('loggedDietomateUserId', userObject._id)
   localStorage.setItem('loggedDietomateUsername', userObject.name)
   localStorage.setItem('loggedDietomateUserEmail', userObject.email)
   localStorage.setItem('loggedDietomateUserToken', userObject.token)
 }
 
 const removeUserFromLocalStorage = () => {
+  localStorage.removeItem('loggedDietomateUserId')
   localStorage.removeItem('loggedDietomateUsername')
   localStorage.removeItem('loggedDietomateUserEmail')
   localStorage.removeItem('loggedDietomateUserToken')
