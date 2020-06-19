38e52227726fd14cf1074f63e448dbbffa7115bd
Or Hadad <orh@wix.com>
2020-06-20 02:44:31 +0300

    add some colors to home title

--- src/components/HomeTitle.js
+++ src/components/HomeTitle.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
+const dietomateGreen = {
+  color: 'rgb(33, 186, 69)',
+}
 // Who knows when we might need a 1337 ReaCtiVe sUpEr home title?
 const HomeTitle = () => {
   return (
     <div className='home-title'>
-      <h1>Automate Your Diet</h1>
+      <h1 style={dietomateGreen}>Automate Your Diet</h1>
       <h2>Control Your Health</h2>
-      <h4>Dietomate creates an optimal nutrition plan for you, so that you don't depend on nutritionists, fitness coaches and diaries
+      <h4 style={dietomateGreen}>Dietomate creates an optimal nutrition plan for you, so that you don't depend on nutritionists, fitness coaches and diaries
       <br></br>
       All you have left is just the eating!</h4>
     </div>
